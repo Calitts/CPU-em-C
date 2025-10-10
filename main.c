e161ae7ecc3d233fd6afe3445e68edf819301326
Calks <68165466+Calitts@users.noreply.github.com>
2025-10-10 00:55:08 -0300

    Adição dos Arquivo iniciais

---
+++ main.c
@@ -0,0 +1,20 @@
+// Arquivo Driver para Entrada e Saída
+
+#include <stdio.h>
+#include "registradores.c"
+
+int main(){
+    registrador pc;
+    pc.tipo = "pc";
+    
+    registrador mbr;
+    mbr.tipo = "mbr";
+
+    registrador mar;
+    mar.tipo = "mar";
+
+    registrador ir;
+    ir.tipo = "ir";
+
+    
+}
---
+++ registradores.c
@@ -0,0 +1,8 @@
+// Arquivo para criação e edição de registradores
+
+#include <string.c>
+
+typedef struct registrador {
+    char tipo[15];
+    int valor;
+};
