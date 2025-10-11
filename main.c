331f85b02439acc12998863c5c71e62772cd24e0
Calitts <cauapittalima@gmail.com>
2025-10-11 12:31:31 -0300

    Removi arquivos extras e decidi organizar tudo em um só arquivo

--- main.c
+++ main.c
@@ -1,20 +1,54 @@
-// Arquivo Driver para Entrada e Saída
-
 #include <stdio.h>
-#include "registradores.c"
+#include <string.h>
 
-int main(){
-    registrador pc;
-    pc.tipo = "pc";
-    
-    registrador mbr;
-    mbr.tipo = "mbr";
+// CONSTANTES
 
-    registrador mar;
-    mar.tipo = "mar";
+#define NUMERO_REGISTRADORES_ESPECIAIS 6
+#define NUMERO_REGISTRADORES_GERAIS 5
+#define TAMANHO_MEMORIA 1<<20 // MEMORIA 1 MB
 
-    registrador ir;
-    ir.tipo = "ir";
+// REGISTRADORES
+typedef struct {
+    char tipo[15];
+    int valor;
+} registrador;
 
-    
+
+// ULA
+
+void ADD(registrador c, registrador a, registrador b) {
+    c.valor = a.valor + b.valor;
+}
+
+void SUB(registrador c, registrador a, registrador b) {
+    c.valor = a.valor - b.valor;
+}
+
+void MUL(registrador c, registrador a, registrador b) {
+    c.valor = a.valor * b.valor;
+}
+
+void DIV(registrador c, registrador a, registrador b) {
+    c.valor = a.valor / b.valor;
+}
+
+
+// ENTRADA E SAÍDA + DRIVER
+
+int main() {
+    // DEFINIÇÃO REGISTRADORES ESPECIAIS E DE USO GERAL
+    char registradores_speciais[NUMERO_REGISTRADORES_ESPECIAIS][20] = { "pc", "mbr", "mar", "ir" , "mq", "ac"};
+    registrador especiais[NUMERO_REGISTRADORES_ESPECIAIS];
+    registrador gerais[NUMERO_REGISTRADORES_GERAIS]; 
+
+    for (int i = 0; i < NUMERO_REGISTRADORES_ESPECIAIS; i++) {
+        strcpy(especiais[i].tipo, registradores_speciais[i]);
+    }
+    printf(especiais[0].tipo);
+
+    // DEFINIÇÃO DE MEMORIA
+    int memoria[TAMANHO_MEMORIA];
+
+    // ENTRADA
+
 }
--- registradores.c
+++
@@ -1,8 +0,0 @@
-// Arquivo para criação e edição de registradores
-
-#include <string.c>
-
-typedef struct registrador {
-    char tipo[15];
-    int valor;
-};
