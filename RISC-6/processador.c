5da3be12d6fad98b03e8977d20900b28c0196008
cesari03 <cesario.0324@hotmail.com>
2026-01-08 14:37:52 -0300

    Versão 0.1

--- RISC-6/processador.c
+++ RISC-6/processador.c
@@ -4,9 +4,31 @@
 #include <string.h>
 #include <stdbool.h>
 
+// Intruções
+#define JMP_I           (0x0)
+#define JEQ_I           (0x1)
+#define JNE_I           (0x2)
+#define JLT_I           (0x3)
+#define JGE_I           (0x4)
+#define LDR_Rd          (0x2)
+#define STR_Rn          (0x3)
+#define MOV_Rd          (0x4)
+#define ADD_Rd          (0x5)
+#define ADDI            (0x6)
+#define SUB_Rd          (0x7)
+#define SUBI            (0x8)
+#define AND             (0x9)
+#define OR              (0xA)
+#define SHR             (0xB)
+#define SHL             (0xC)
+#define CMP             (0xD)
+#define PUSH            (0xE)
+#define POP             (0xF)
+#define HALT            (0xF)
+
 int main(){
 
-    
+
 
     return 0;
 }
